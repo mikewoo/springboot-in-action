f0cf25c0d4f07146fe82355a77d8abf864c81ce9
mingming.gui <ahqsgmm@163.com>
2018-06-08 15:20:34 +0800

    Ieda开启Springboot热部署

---
+++ springboot-hello/src/main/java/com/mikewoo/study/domain/User.java
@@ -0,0 +1,46 @@
+package com.mikewoo.study.domain;
+
+/**
+ * 用户实体类
+ *
+ * @auther Phantom Gui
+ * @date 2018/6/8 15:04
+ */
+public class User {
+
+    private String id;
+
+    private String name;
+
+    private int age;
+
+    public User(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- springboot-hello/src/main/java/com/mikewoo/study/web/HelloController.java
+++ springboot-hello/src/main/java/com/mikewoo/study/web/HelloController.java
@@ -1,9 +1,38 @@
 package com.mikewoo.study.web;
 
+import com.mikewoo.study.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ Ieda Springboot 热部署
+ 1. 添加依赖和热部署插件
+     <dependency>
+         <groupId>org.springframework.boot</groupId>
+         <artifactId>spring-boot-devtools</artifactId>
+         <optional>true</optional>
+     </dependency>
+     <build>
+         <plugins>
+             <plugin>
+                 <groupId>org.springframework.boot</groupId>
+                 <artifactId>spring-boot-maven-plugin</artifactId>
+                 <configuration>
+                    <fork>true</fork>//该配置必须
+                 </configuration>
+             </plugin>
+         </plugins>
+     </build>
+ 2. 开启idea自动make功能
+     （1） CTRL + SHIFT + A --> 查找make project automatically --> 选中
+     （2） CTRL + SHIFT + A --> 查找Registry --> 找到并勾选compiler.automake.allow.when.app.running
+ 3. Chrome禁用缓存
+     打开Chrome， 按F12（或Ctrl+Shift+J或Ctrl+Shift+I）， 选择NetWork --> Disable Cache(while DevTools is open)
+ 4. 重启IDEA
+ 至此，在idea中就可以愉快的修改代码了，修改后可以及时看到效果，无须手动重启和清除浏览器缓存。
+ *
  * HelloController
  *
  * @auther Phantom Gui
@@ -12,13 +41,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "hello springboot";
     }
 
-    @RequestMapping("/sayHello")
+    @GetMapping("/sayHello")
     public String sayHello(String name) {
         return "hello, " + name;
     }
+
+    @GetMapping("/user/{id}")
+    public User user(@PathVariable("id") String id) {
+        User user = new User(id, "Phantom", 28);
+        return user;
+    }
+
+
 }
