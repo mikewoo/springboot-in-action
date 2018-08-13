d134d0215ebdef809b413461678b1dc3e869ddc4
Eric Gui <ahqsgmm@163.com>
2018-08-13 19:46:53 +0800

    Springboot 整合 Apache Shiro 实现权限控制

--- springboot-shiro/src/main/java/com/mikewoo/study/web/TestController.java
+++ springboot-shiro/src/main/java/com/mikewoo/study/web/TestController.java
@@ -6,11 +6,9 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
